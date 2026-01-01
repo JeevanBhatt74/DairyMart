8d7d6aab0c908e8c881975e5ba2147e81dda7b6a
JeevanBhatt74 <iamjeevanrkb@gmail.com>
2026-01-01 18:18:21 +0545

    docs: added comments to Hive database service

--- lib/core/services/hive/hive_service.dart
+++ lib/core/services/hive/hive_service.dart
@@ -1,3 +1,4 @@
+// Handles local database storage
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import '../../../features/auth/data/models/user_model.dart';
